385c3bee9c5c27ae652958676361a20ae6edc618
Carman Fu <huivioemp1bs@posteo.com>
2026-05-03 10:29:20 +0800

    chore(flake/nixpkgs): switch to nixpkgs-unstable branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Comma-leading JSON formatter";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
